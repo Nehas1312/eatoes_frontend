c6bebe97842536a98c5cbbde45b2449b8ec93fba
Nehas1312 <nehaskandiraju@gmail.com>
2025-04-28 17:47:34 +0530

    changing the api

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";  // your backend URL
+const API_BASE_URL = "https://eatoes-backend-ql1e.onrender.com/api";  // your backend URL
 
 // Fetch all menu items
 export const getMenuItems = async () => {
